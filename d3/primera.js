7ac0f98bce818b1f8e1088a3d33e5a79f68687dc
Valeria Montenegro Castro <37454225+valeriamc21@users.noreply.github.com>
2018-04-20 18:17:25 -0300

    Segunda evaluación
    :c lo intenté

---
+++ d3/primera.js
@@ -0,0 +1,9 @@
+//aquí debes trabajar con datos de un arreglo o un objeto, incluido directamente en la variable data.
+var data;
+var w = 450;
+var h = 450;
+d3.select(".dataviz-primera")
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h )
+	.style("background","#EEEEEE");
